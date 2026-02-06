2a9e92b4d781fc2d23f566bcc09913a5450776d1
francisshere <francisadriangapol18@gmail.com>
2026-02-06 13:48:39 +0800

    donepracticearray

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function filterRange(arr, a, b) {
-  return arr.filter(item => (a <= item && item <= b));
+  return arr.filter(item =>(item >= a && item <= b))
 }
 
 let arr = [5, 3, 8, 1];
 
-let filtered = filterRange(arr, 1, 4);
+let filtered = filterRange(arr, 1, 5);
 
 alert( filtered ); // 3,1 (matching values)
 
