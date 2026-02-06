00c77f738f5ab390f2f25838d3fd587f88d09f0d
francisshere <francisadriangapol18@gmail.com>
2026-02-06 13:36:43 +0800

    donepracticearray

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-let sum = 0;
-
-function sumInput() {
-    let arr = [];
-
-    while (true) {
-        let value = prompt("Enter a number:", 0);
-
-        // Break the loop if the input is not a finite number
-        if (value === null || value === "" || !isFinite(value)) 
-            {break;} else {
-                arr.push(value);
-
-                sum += +value;
-                
-                console.log(arr);
-                console.log(sum);
-            }
-    }
+function camelize(str){
+    removed_ = str.split("-");
+    capitalized_ = removed_.map((word, index) => {
+        if (index == 0) {
+            return word;
+        } else {
+            return word[0].toUpperCase() + word.slice(1);
+        }
+    });
+    joined_ = capitalized_.join("");
+    alert (joined_);
 }
 
-sumInput();
+camelize("-webkit-transition");
