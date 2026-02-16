de9ae7c6f626848acf218eb46904563e38434e98
francisshere <francisadriangapol18@gmail.com>
2026-02-17 00:26:21 +0800

    1st task

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
+const p = document.querySelector("#red-p");
+p.classList.add("paragraph");
+p.textContent = "Hey I'm red!";
+p.style.color = "red";
+
+
+
 container.appendChild(content);
+container.appendChild(p);
