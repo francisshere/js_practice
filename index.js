d58b1fc17eff93e3355065604086ebb583c53104
francisshere <francisadriangapol18@gmail.com>
2026-02-17 00:36:19 +0800

    3rd task

--- index.js
+++ index.js
@@ -18,10 +18,22 @@ h3.style.color = "blue";
 
 const div = document.createElement("div");
 div.classList.add("black-pink-div");
-div.style.backgroundColor = "pink";
+div.style.backgroundColor = "red";
 div.style.border = "1px solid black";
+
+const h1 = document.createElement("h1");
+h1.classList.add("div-heading");
+h1.textContent = "Im in a div";
+
+const p2 = document.createElement("p");
+p2.classList.add("div-paragraph");
+p2.textContent = "ME TOO!";
 
 container.appendChild(content);
 container.appendChild(p);
 container.appendChild(h3);
+container.appendChild(container2);
+
 container2.appendChild(div);
+container2.appendChild(h1);
+container2.appendChild(p2);
