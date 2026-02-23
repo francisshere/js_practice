97d69d06711a2a50c6aae329a2a96032afca927b
francisshere <francisadriangapol18@gmail.com>
2026-02-24 00:04:40 +0800

    third js exec

---
+++ cisco_jsfr.js
@@ -0,0 +1,15 @@
+let name1 = "Maxwell Wright";
+let name2 = "Raja Villarreal";
+let name3 = "Halen Richards";
+
+let phone1 = "(0191) 719 6495";
+let phone2 = "0866 398 2895";
+let phone3 = "0800 111";
+
+let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+let email2 = "posuere.vulputate@sed.com";
+let email3 = "libero@convallis.edu";
+
+console.log("Name: " + name1 + " / Phone: " + phone1 + " / Email: " + email1);
+console.log("Name: " + name2 + " / Phone: " + phone2 + " / Email: " + email2);
+console.log("Name: " + name3 + " / Phone: " + phone3 + " / Email: " + email3);
