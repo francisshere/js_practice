180d034a005cb35f3d0b0f2a454ce3b33c4afa2a
francisshere <francisadriangapol18@gmail.com>
2026-03-03 15:59:05 +0800

    done 3rd of section-prac 2.1.11

--- section-practice.js
+++ section-practice.js
@@ -1,27 +1,6 @@
-let b1 = true;
-let b2 = Boolean(true);
+let string_ver = "1234";
+let number_ver = Number(string_ver);
+let bigInt_ver = BigInt(number_ver);
+let boolean_ver = Boolean(bigInt_ver);
 
-let n1 = 100;
-let n2 = Number(200);
-
-let bi1 = 100n;
-let bi2 = BigInt(200);
-
-let s1 = "Hello";
-let s2 = String("Hello");
-
-let u1 = undefined;
-
-console.log(`${b1} [${typeof b1}]`);
-console.log(`${b2} [${typeof b2}]`);
-
-console.log(`${n1} [${typeof n1}]`);
-console.log(`${n2} [${typeof n2}]`);
-
-console.log(`${bi1} [${typeof bi1}]`);
-console.log(`${bi2} [${typeof bi2}]`);
-
-console.log(`${s1} [${typeof s1}]`);
-console.log(`${s2} [${typeof s2}]`);
-
-console.log(`${u1} [${typeof u1}]`);
+console.log(boolean_ver);
