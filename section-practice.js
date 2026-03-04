14550c0d76273689972deefbb47b65b1bd3cd7a8
francisshere <francisadriangapol18@gmail.com>
2026-03-04 17:35:53 +0800

    done 6th practice session of 2.1.11

--- section-practice.js
+++ section-practice.js
@@ -1,13 +1,3 @@
-let num1 = 10;
-let num2 = 20;
-let str1 = "a";
-let str2 = "b";
-let big1 = 10n;
-let big2 = 20n;
-let bool1 = true;
-let bool2 = false;
+const str1 = 42 + Number("1");
 
-console.log(num1 + num2);
-console.log(str1 + str2);
-console.log(big1 + big2);
-console.log(bool1 + bool2);
+console.log(str1);
