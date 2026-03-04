d14891638e28da24dc704cb0ef13fda05701b676
francisshere <francisadriangapol18@gmail.com>
2026-03-04 17:48:23 +0800

    done lab of session 2

--- cisco_jsfr.js
+++ cisco_jsfr.js
@@ -1,15 +1,25 @@
-let name1 = "Maxwell Wright";
-let name2 = "Raja Villarreal";
-let name3 = "Halen Richards";
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+},
 
-let phone1 = "(0191) 719 6495";
-let phone2 = "0866 398 2895";
-let phone3 = "0800 111";
+{
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com"
 
-let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
-let email2 = "posuere.vulputate@sed.com";
-let email3 = "libero@convallis.edu";
+},
 
-console.log("Name: " + name1 + " / Phone: " + phone1 + " / Email: " + email1);
-console.log("Name: " + name2 + " / Phone: " + phone2 + " / Email: " + email2);
-console.log("Name: " + name3 + " / Phone: " + phone3 + " / Email: " + email3);
+{
+    name: "Halen Richards",
+    phone: "0800 111",
+    email: "libero@convallis.edu"
+}
+];
+
+
+
+console.log("Name: " + contacts[0].name + " / Phone: " + contacts[0].phone + " / Email: " + contacts[0].email);
+console.log("Name: " + contacts[1].name + " / Phone: " + contacts[1].phone + " / Email: " + contacts[1].email);
+console.log("Name: " + contacts[2].name + " / Phone: " + contacts[2].phone + " / Email: " + contacts[2].email);
