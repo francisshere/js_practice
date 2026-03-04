9ce6b02a6882a2108bb43e67e9adf6c26b138cea
francisshere <francisadriangapol18@gmail.com>
2026-03-04 17:27:23 +0800

    done 4th  of section-prac 2.1.11

--- section-practice.js
+++ section-practice.js
@@ -1,6 +1,13 @@
-let string_ver = "1234";
-let number_ver = Number(string_ver);
-let bigInt_ver = BigInt(number_ver);
-let boolean_ver = Boolean(bigInt_ver);
+let num1 = 10;
+let num2 = 20;
+let str1 = "a";
+let str2 = "b";
+let big1 = 10n;
+let big2 = 20n;
+let bool1 = true;
+let bool2 = false;
 
-console.log(boolean_ver);
+console.log(num1 + num2);
+console.log(str1 + str2);
+console.log(big1 + big2);
+console.log(bool1 + bool2);
