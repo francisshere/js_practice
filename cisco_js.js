633e0172cade01fd7c020e17a77be52f0aa5707c
francisshere <francisadriangapol18@gmail.com>
2026-03-20 11:34:23 +0800

    done session prac 3.0.10

--- cisco_js.js
+++ cisco_js.js
@@ -1,5 +1,4 @@
-console.log(4 * 5 === 20);
-console.log(6 * 5 == "30");
-console.log(-17 < 0);
-console.log(25 > 1);
-console.log(2 + 2 * 2 > 4);
+console.log(true || false);  
+console.log(false || !false);  
+console.log(false && false || true);  
+console.log(true || false || false && true);
