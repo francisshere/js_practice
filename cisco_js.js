86271ccd871191c6067ce7be78cae0e0f889c711
francisshere <francisadriangapol18@gmail.com>
2026-03-20 11:22:18 +0800

    done session prac 2.2.3

--- cisco_js.js
+++ cisco_js.js
@@ -1,22 +1,22 @@
 let books = [
     {title: "Speaking JavaScript",
      author: "Axel Rauschmayer",
-     pages: "460",
+     pages: 460,
     },
 
     {title: "Programming JavaScript Applications",
      author: "Eric Elliott",
-     pages: "254",
+     pages: 254,
     },
 
     {title: "Understanding ECMAScript 6",
      author: "Nicholas C. Zakas",
-     pages: "352",
+     pages: 352,
     }    
 ];
 
 //2
-books.push({title: "Learning JavaScript Design Patterns", author: "Addy Osmani", pages:"254"});
+books.push({title: "Learning JavaScript Design Patterns", author: "Addy Osmani", pages:254});
 console.log(books.length);
 console.log(books[0].title);
 console.log(books[1].title);
@@ -34,3 +34,7 @@ console.log(books.length);
 console.log(books[0].title);
 console.log(books[1].title);
 console.log(books[2].title);
+
+//5 
+let sum = books[0].pages + books[1].pages + books[2].pages;
+console.log(sum);
