697799e6bae880ae46055a5000f6e62b6f0a9ca9
francisshere <francisadriangapol18@gmail.com>
2026-03-20 10:48:00 +0800

    done session prac 2.2.3

--- cisco_js.js
+++ cisco_js.js
@@ -1,22 +1,8 @@
-const rose_prc = 8;
-const lily_prc = 10;
-const tulip_prc = 2;
+let ticket = {
+    from: "Station 1",
+    to: "Station 2",
+    price: "$5"
+};
 
-let rose_qty = 70
-let lily_qty = 50;
-let tulip_qty = 120;
 
-rose_qty = rose_qty - 20;
-lily_qty = lily_qty - 30;
-
-let rose_total = rose_prc * rose_qty;
-let lily_total = lily_prc * lily_qty;
-let tulip_total = tulip_prc * tulip_qty;
-
-let total_cost = rose_total + lily_total + tulip_total;
-
-console.log("Rose – unit price:" + rose_prc + ", quantity: " + rose_qty + ", value: " + rose_total);
-console.log("Lily – unit price:" + lily_prc + ", quantity: " + lily_qty + ", value: " + lily_total);
-console.log("Tulip – unit price:" + tulip_prc + ", quantity: " + tulip_qty + ", value: " + tulip_total);
-console.log("Total: " + total_cost);
-
+console.log("From: " + ticket.from + " / To: " + ticket.to + " / Price: " + ticket.price);
