a288052bd656b1be4a4f29c239a209182c511239
francisshere <francisadriangapol18@gmail.com>
2026-03-20 11:16:48 +0800

    done session prac 2.2.3

--- cisco_js.js
+++ cisco_js.js
@@ -15,9 +15,22 @@ let books = [
     }    
 ];
 
+//2
 books.push({title: "Learning JavaScript Design Patterns", author: "Addy Osmani", pages:"254"});
 console.log(books.length);
 console.log(books[0].title);
 console.log(books[1].title);
 console.log(books[2].title);
 console.log(books[3].title);
+
+//3
+let last_two = books.slice(-2);
+console.log(last_two[0].title);
+console.log(last_two[1].title);
+
+//4
+books.shift();
+console.log(books.length);
+console.log(books[0].title);
+console.log(books[1].title);
+console.log(books[2].title);
