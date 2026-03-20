4139af1235f9b41a7aa33062e321db6f9dac7c49
francisshere <francisadriangapol18@gmail.com>
2026-03-20 11:00:07 +0800

    done session prac 2.2.3

--- cisco_js.js
+++ cisco_js.js
@@ -1,7 +1,16 @@
-let person = {};
-person.name = "Francis";
-person.surname = "Gapol";
+let books = [
+    {title: "Speaking JavaScript",
+     author: "Axel Rauschmayer",
+     pages: "460",
+    },
 
+    {title: "Programming JavaScript Applications",
+     author: "Eric Elliott",
+     pages: "254",
+    },
 
-console.log("Name: " + person.name);
-console.log("Surname: " + person.surname);
+    {title: "Understanding ECMAScript 6",
+     author: "Nicholas C. Zakas",
+     pages: "352",
+    }    
+];
