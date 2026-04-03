229e54e375d1c08405109df022cd3c6f9e9fdac8
francisshere <francisadriangapol18@gmail.com>
2026-04-04 00:05:26 +0800

    doneprac

--- cisco_js.js
+++ cisco_js.js
@@ -1,6 +1,8 @@
-let width = Number(prompt("width: "));
-let height = Number(prompt("height: "));
-let length = Number(prompt("length: "));
-
-let volume = width * height * length;
-console.log(`Volume is ${volume}`);
+try {
+    let testArray1 = Array(10);
+    console.log(testArray1.length); // -> 10
+    let testArray2 = Array(-1); // -> Uncaught RangeError: Invalid array length
+    console.log(testArray2.length);
+} catch (err) {
+    console.log("Error is: " + err);
+}
