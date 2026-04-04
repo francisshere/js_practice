c5d1a78500bddd8eea3ed3c060c2c0fe720605c9
francisshere <francisadriangapol18@gmail.com>
2026-04-04 14:50:28 +0800

    done lab prac

--- cisco_js.js
+++ cisco_js.js
@@ -1,29 +1,14 @@
-let movies = [
-];
-
-
-while (true){
-    let movie_input = {
-        movie_name: prompt("Movie name: "),
-        movie_rating: Number(prompt("Movie rating: ")),
+let vessel = [
+    {
+        LATITUDE: 40.07288, 
+        LONGITUDE: 154.48535, 
+        COURSE: 285.6, 
+        SPEED: 14.0, 
+        IMO: 9175717,
+        NAME: "MARENO",
     }
-    movies.push(movie_input);
-
-    if (movie_input.movie_name === null || movie_input.movie_rating === null) {
-        console.log("Movies with Ratings Above 7");
-        movies.forEach(element => {
-        if (element.movie_rating > 7){
-            console.log(element.movie_name + ": " + element.movie_rating); 
-        }
-        });
-
-        console.log("Movies with Ratings below 7");
-        movies.forEach(element => {
-        if (element.movie_rating < 7){
-            console.log(element.movie_name + ": " + element.movie_rating); 
-        }
-        });
-
-        return;
-    }
+]
+ 
+for( let key in vessel) { 
+    console.log(`${key} -> ${vessel[key]}`); 
 }
