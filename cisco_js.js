332bff9603e02b7c5af7da7f528660e06ff265de
francisshere <francisadriangapol18@gmail.com>
2026-04-04 13:12:00 +0800

    done lab practice

--- cisco_js.js
+++ cisco_js.js
@@ -1,6 +1,2 @@
 value = prompt("Enter a number from 1 - 110: ");
-if (value >= 90 && value <= 110){
-    console.log("Bingo!");
-} else {
-    console.log("Miss!");
-}
+(value >= 90 && value<= 110) ? alert("Bingo!") : alert ("Miss!");
