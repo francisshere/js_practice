540bc2ba4f640bc9e94b86cced1c3ef039303311
francisshere <francisadriangapol18@gmail.com>
2026-04-04 22:55:26 +0800

    done task 1a

---
+++ cisco_prac.js
@@ -0,0 +1,11 @@
+let numbers = [50, 10, 40, 30, 20];
+
+let compareNum = (a,b) => {
+    let retVal = (a < b) ? -1 : 1;
+    return retVal
+}
+
+let sorted = numbers.sort(compareNum);
+console.log(sorted); // [10, 20, 30, 40, 50]
+
+
