c95a32aabe1df634bd25d58aa16659ad46ea3135
francisshere <francisadriangapol18@gmail.com>
2026-04-04 14:18:45 +0800

    done lab prac

--- cisco_js.js
+++ cisco_js.js
@@ -1,5 +1,11 @@
-n = 100
-do {
-    console.log(n);
-    n -= 10;
-} while (n > -1);
+let n = Number(prompt("Enter an initial number: "));
+let n2 = Number(prompt("Enter the final number: "));
+
+if (n < n2){
+    console.log("Wrong inputs");
+} else {
+    do {
+        console.log(n);
+        n -= 10;
+    } while (n > -1);
+}
