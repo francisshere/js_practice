d75a5f472e63c877882e25f081857be2399cd5c7
unknown <francisshere>
2026-04-05 23:33:13 +0800

    shortened task 6

--- cisco_prac.js
+++ cisco_prac.js
@@ -1,6 +1,4 @@
-function fibRec(n){
-    return n<2?n:fibRec(n-1) + fibRec(n-2);
-}
+let fibRec = n => n<2?n:fibRec(n-1) + fibRec(n-2);
 
-console.log(fibbRec(4)); // -> 3
-console.log(fibbRec(7)); // -> 13
+console.log(fibRec(4)); // -> 3
+console.log(fibRec(7)); // -> 13
